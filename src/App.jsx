b5f388440c6d7b09c245296cc534ed13c0c92775
haritha037 <e20037@eng.pdn.ac.lk>
2024-10-09 21:54:28 +0530

    moved Input and Button to their own files

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: #f7f7a0;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  background-color: #721242;
-  border-radius: 7px;
-  color: white;
-  /* cursor: pointer; */
-  margin: 20px;
-`;
-
-const Input = styled.input`
-  font-size: 16px;
-  /* background-color: #fdf; */
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
 `;
 
 const StyledApp = styled.div`
