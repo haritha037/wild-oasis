18cfc7be0277b8400c46a4a7b93b47a6d89b2654
haritha037 <e20037@eng.pdn.ac.lk>
2024-10-09 20:45:46 +0530

    created GlobalStyles component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,49 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: #f7f7a0;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  background-color: #721242;
+  border-radius: 7px;
+  color: white;
+  /* cursor: pointer; */
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  font-size: 16px;
+  /* background-color: #fdf; */
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: #1b0d27;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Hello world</H1>
+        <Button onClick={() => alert("check in")}>Check in</Button>
+        <Button onClick={() => alert("check in")}>Check out</Button>
+        <Input type="date" placeholder="Number of guests"></Input>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
